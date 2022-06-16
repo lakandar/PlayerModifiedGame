a904d1787eee315c9476f053c0c37fb25e3fd88b
Abu Hasan <lakandar@gmail.com>
2022-06-16 21:28:38 +0600

    Add All First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//selector
+let inputElement=document.querySelector("#inputScore");
+let winScoreElement=document.querySelector("#winningScore");
+let p1dElement=document.querySelector("#p1dice");
+let p2dElement=document.querySelector("#p2dice");
+let diceElement=document.querySelector("#dice");
+let p1btnElement=document.querySelector("#p1btn");
+let p2btnElement=document.querySelector("#p2btn");
+let resetbtnElement=document.querySelector("#resetbtn");
+
+//data
+let winScore=5;
+
+//set default winning score 
+winScoreElement.textContent=winScore;
+
+inputElement.addEventListener("keypress", (e)=>{
+    if(e.key==="Enter"){
+        let inScore=inputElement.value
+        winScoreElement.textContent=inScore;
+        winScore=+inScore;
+        inScore="";
+        //console.log(inputElement.textContent)
+    }
+})
